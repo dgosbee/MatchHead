3608aec1e6d64bae18b704fb522335586c08f4b4
Scott Hommel <shommel@users.noreply.github.com>
2015-02-27 17:44:14 -0500

    Changed comment from SearchProto to MatchHead.

--- src/matchhead/page/Pageable.java
+++ src/matchhead/page/Pageable.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2015 Scott A. Hommel
  *
- * This file is part of SearchProto.
+ * This file is part of MatchHead .
  *
  * SearchProto is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
