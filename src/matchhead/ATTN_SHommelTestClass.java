311c518af74d20d5df22e27d75c2c25d7dd66d87
dain <dgosbee@users.noreply.github.com>
2015-03-02 09:59:53 +0100

    This is my NB/GitHub round trip test.  "ATTN_SHommelTestClass.java" was the only addition for this test.  I wanted to air on the cautious side.

---
+++ src/matchhead/ATTN_SHommelTestClass.java
@@ -0,0 +1,17 @@
+/**
+ * This is nothing more than me testing the GitHub/NB round trip
+ * 
+ */
+
+package matchhead;
+
+/**
+ *
+ * @author dain
+ */
+
+public class ATTN_SHommelTestClass {
+
+    String testStr = "Hopefully this works";
+    
+}
