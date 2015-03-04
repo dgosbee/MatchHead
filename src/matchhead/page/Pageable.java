0d2e2bc7e1cbbc007b86af050d87b58edf30554f
dain <dgosbee@users.noreply.github.com>
2015-03-04 12:58:47 +0100

    I changed the first comment block wording from "SearchProto" to "MatchHead".  I also added comments to the classes in the entire outputformatter folder.

--- src/matchhead/page/Pageable.java
+++ src/matchhead/page/Pageable.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2015 Scott A. Hommel
  *
- * This file is part of MatchHead .
+ * This file is part of MatchHead.
  *
- * SearchProto is free software: you can redistribute it and/or modify
+ * MatchHead is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * SearchProto is distributed in the hope that it will be useful,
+ * MatchHead is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with SearchProto.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MatchHead.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package matchhead.page;
