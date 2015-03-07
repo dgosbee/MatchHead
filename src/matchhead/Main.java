86eb0a1fc51b5e6362e1a017ad7d8d0f7fd0a54f
shommel <shommel@localhost.localdomain>
2015-03-07 05:40:09 -0500

    Renamed MatchHead to Main

--- src/matchhead/Main.java
+++ src/matchhead/Main.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2015 Scott A. Hommel
  *
- * This file is part of MatchHead.
+ * This file is part of Main.
  *
- * MatchHead is free software: you can redistribute it and/or modify
+ * Main is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MatchHead is distributed in the hope that it will be useful,
+ * Main is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with MatchHead.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Main.  If not, see <http://www.gnu.org/licenses/>.
  */
 package matchhead;
 
@@ -36,7 +36,7 @@ import matchhead.prefilter.QueryPreFilter;
 import matchhead.prefilter.QueryPreFilterException;
 import matchhead.prefilter.QueryPreFilterable;
 
-public class MatchHead {
+public class Main {
 
     private static String query = "what are packages?"; // Simulates search query from user
     private static MatchPhraseDataFileLoadable loader;
